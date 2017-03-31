e7ea6b9e891b244316382577f999bd3388772844
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-03-30 21:19:28 -0400

    Define what happens in console based on result of AJAX request

--- assets/scripts/books/ui.js
+++ assets/scripts/books/ui.js
@@ -1,5 +1,15 @@
 'use strict'
 
-module.exports = {
+const onSuccess = function (data) {
+  console.log('AWESOME!! SUCCESS!!')
+  console.log('data is ', data)
+}
 
+const onFailure = function () {
+  console.log('Sad face.')
+}
+
+module.exports = {
+  onSuccess,
+  onFailure
 }
