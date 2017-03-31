0e0582b3f661a05de9ca808e8b89814ca92d1cfc
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-03-30 21:19:05 -0400

    Define click handlers

--- assets/scripts/books/events.js
+++ assets/scripts/books/events.js
@@ -1,11 +1,25 @@
 'use strict'
 
+const booksApi = require('./api.js')
+const booksUi = require('./ui.js')
 // get in the habit of naming your handlers, it eases debugging.
 //
 // also, follow a convention for handlers. here, I name my handler
 // beginning with 'on' to denote that it is done when the GET /books
 // button is clicked
 
-module.exports = {
+const onGetBooks = function (event) {
+  event.preventDefault()
+  console.log('You clicked the books button')
+  booksApi.index()
+    // does something
+    // then, we have a method chain
+    // these are UI concerns
+  .then(booksUi.onSuccess)
+  .catch(booksUi.onFailure)
+}
 
+// Export this object
+module.exports = {
+  onGetBooks: onGetBooks
 }
