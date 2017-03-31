322dde32002d0795caad05b41d8c85c964d36c9e
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-03-30 21:18:13 -0400

    Add AJAX commands

--- assets/scripts/books/api.js
+++ assets/scripts/books/api.js
@@ -1,5 +1,16 @@
 'use strict'
 
-module.exports = {
+const app = require('../app.js')
 
+const index = function () {
+  console.log('booksapi index ran')
+  // AJAX takes some methods in the form of an object
+  return $.ajax({
+    url: app.host + '/books',
+    method: 'GET'
+  })
+}
+
+module.exports = {
+  index
 }
